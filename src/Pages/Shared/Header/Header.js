968397fd9eb5e558b46f1b9e00c2c62405a93339
unknown <latapata1952@gmail.com>
2022-04-30 00:31:02 +0600

    header added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo.PNG';
+import './Header.css';
+
+const Header = () => {
+
+    return (
+        <header className='header'>
+            <Navbar collapseOnSelect expand="lg" sticky='top'>
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img height={60} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className='text-center' id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Link to='/home'>Home</Link>
+                            <Link to='/blogs'>Blogs</Link>
+                            <Link to='/about'>About</Link>
+                            <Link to='/checkout'>Checkout</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
