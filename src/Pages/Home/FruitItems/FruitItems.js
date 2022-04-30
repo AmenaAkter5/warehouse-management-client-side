0d07c53488be033d99c1f395a69abbd54944d3ab
unknown <latapata1952@gmail.com>
2022-04-30 23:58:15 +0600

    inventory added

--- src/Pages/Home/FruitItems/FruitItems.js
+++ src/Pages/Home/FruitItems/FruitItems.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './FruitItems.css';
 
-const FruitItems = () => {
+
+const FruitItems = ({ item }) => {
+    const { name, price, description, img } = item
+
     return (
-        <div>
-            <h1>These are fruit Items</h1>
+        <div className="user-reviews px-3">
+            <div className="d-flex justify-content-center pt-3 mb-3">
+                <img src={img} alt="" />
+            </div>
+            <h5 className="text-center fw-bold ratings">{price}</h5>
+            <h4 className="text-center mb-3">{name}</h4>
+            <p>{description}</p>
         </div>
     );
 };
--- src/Pages/Home/Inventory/Inventory.js
+++ src/Pages/Home/Inventory/Inventory.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Inventory.css';
+import FruitItems from '../FruitItems/FruitItems';
 
 const Inventory = () => {
+
+    // users data load state
+    const [items, setItems] = useState([]);
+
+    // users data fetch
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setItems(data))
+    }, [])
+
     return (
-        <div>
-            <h1>This is Inventory</h1>
-        </div>
+        <section className='inventory-section'>
+            <div className="container mx-auto my-5">
+                <div className="title-text">
+                    <p>Inventory</p>
+                    <h1>The New Arrival</h1>
+                </div>
+                <div className="items-container">
+                    {
+                        items.map(item => <FruitItems
+                            key={items._id}
+                            item={item}
+                        ></FruitItems>)
+                    }
+                </div>
+            </div>
+        </section>
     );
 };
 
