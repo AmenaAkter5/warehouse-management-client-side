1576dc79fc1b53611bef846c93fef95a52a2269b
unknown <latapata1952@gmail.com>
2022-05-08 15:00:54 +0600

    package area updated

--- src/Pages/Home/PackageArea/PackageArea/PackageArea.js
+++ src/Pages/Home/PackageArea/PackageArea/PackageArea.js
@@ -1,8 +1,6 @@
 import './PackageArea.css';
 import React from 'react';
-import PackageOne from '../PackageOne/PackageOne';
-import PackageTwo from '../PackageTwo/PackageTwo';
-import PackageThree from '../PackageThree/PackageThree';
+import PackageDetail from '../PackageDetail/PackageDetail';
 
 
 const PackageArea = () => {
@@ -14,14 +12,50 @@ const PackageArea = () => {
                 <h1>Our Offered Packages</h1>
             </div>
             <div className='package-container my-5'>
-                <div className="shadow package">
-                    <PackageOne></PackageOne>
+                <div className="shadow package card h-100 px-3 mx-3 border-0 pt-5">
+                    <PackageDetail
+                        key='1'
+                        price='300'
+                        name='Basic'
+                        statement='For the basic management of your products.'
+                        features={
+                            ['Limited space access',
+                                'Secuirty facility',
+                                'Event support',
+                                'Well managed',
+                                'per day bill']
+                        }
+                    ></PackageDetail>
                 </div>
-                <div>
-                    <PackageTwo></PackageTwo>
+                <div className="h-100 p-4 mb-3 mx-3 pt-5 border-0 shadow-lg highlighted text-white">
+                    <PackageDetail
+                        key='2'
+                        price='800'
+                        name='Standard'
+                        statement='For arranging and preserve your products as your demand'
+                        features={
+                            ['Full space access',
+                                'Experienced team',
+                                'Latest technology support',
+                                'Standard management system',
+                                'pay as plan']
+                        }
+                    ></PackageDetail>
                 </div>
-                <div className="shadow package">
-                    <PackageThree></PackageThree>
+                <div className="shadow package card h-100 px-3 mx-3 border-0 pt-5">
+                    <PackageDetail
+                        key='3'
+                        price='1200'
+                        name='Premium'
+                        statement='Storing your products for exporting purpose.'
+                        features={
+                            ['Latest technology is used',
+                                'Maintain international business law',
+                                'Event support',
+                                'Well managed',
+                                'per day bill']
+                        }
+                    ></PackageDetail>
                 </div>
             </div>
         </section>
---
+++ src/Pages/Home/PackageArea/PackageDetail/PackageDetail.js
@@ -0,0 +1,30 @@
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './PakageDetail.css';
+import React from 'react';
+
+const PackageDetail = (props) => {
+
+    const { price, name, statement, features } = props;
+
+    return (
+        <div>
+            <h2>{price}/-</h2>
+            <h3>{name}</h3>
+            <p>{statement}</p>
+            {
+                features.map(feature => <p><FontAwesomeIcon className='me-2 p-1 icon-style' icon={faCheck}></FontAwesomeIcon>{feature}</p>)
+            }
+            <div className="text-center mt-4">
+                <button className="update-btn mb-2">
+                    <a className="button-link-2"
+                        href="https://web.programming-hero.com/">
+                        Book Now
+                    </a>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default PackageDetail;
